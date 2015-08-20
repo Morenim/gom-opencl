bf3b397bc32362535692d01dddab78b6a4085ee8
Willem den Besten <morenimable@gmail.com>
2015-08-20 17:33:57 +0200

    Minor overhaul of the main function: * Uses the flag package to parse commandline arguments. * Flag --cpu added to force OpenCL to run on a CPU device. * OpenCL logic split to a separate function. * GOMEA skeleton code added.

--- kernels/deceptive.cl
+++ kernels/deceptive.cl
@@ -1,7 +1,9 @@
-__kernel void deceptive (__global uint *input, __global uint *output)
-{
-  int v = get_global_id (0);
+__constant int problemLength;
+__constant int populationSize;
+__constant int fosSize;
 
+uint deceptive (__read_only uint solution)
+{
   uint fitness = 0;
   uint k = 4;
   uint numTraps = 32 >> 2;
@@ -12,7 +14,7 @@ __kernel void deceptive (__global uint *input, __global uint *output)
     uint t = 0;
     for (uint j = 0; j < k; j++)
     {
-      t += (z & input[v]) ? 1 : 0;
+      t += (z & solution) ? 1 : 0;
       z <<= 1;
     }
 
@@ -25,6 +27,16 @@ __kernel void deceptive (__global uint *input, __global uint *output)
       fitness += k - t - 1;
     }
   }
+
+  return fitness;
+}
+
+__kernel void gom (__global uint *population, __global uint *fos, __global uint *offspring)
+{
+  int v = get_global_id (0);
   
-  output[v] = fitness;
+  for (uint i = 0; i < 1; i++)
+  {
+    offspring[v] = deceptive(population[v]);
+  }
 }
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/Morenim/gom-opencl/bitset"
 	"github.com/rainliu/gocl/cl"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"unsafe"
+)
+
+var (
+	useCPU bool
 )
 
 func Evaluate(k int, bits bitset.BitSet) (fitness float64) {
@@ -26,7 +32,96 @@ func Evaluate(k int, bits bitset.BitSet) (fitness float64) {
 	return
 }
 
-func main() {
+type byLength [][]int
+
+func (bl byLength) Len() int {
+	return len(bl)
+}
+
+func (bl byLength) Swap(i, j int) {
+	bl[i], bl[j] = bl[j], bl[i]
+}
+
+func (bl byLength) Less(i, j int) bool {
+	if len(bl[i]) < len(bl[j]) {
+		return true
+	}
+	if len(bl[i]) == len(bl[j]) {
+		return bl[i][0] < bl[j][0]
+	}
+	return false
+}
+
+var deviceErrorMap = map[cl.CL_int]string{
+	cl.CL_SUCCESS:             "cl: Success",
+	cl.CL_DEVICE_NOT_FOUND:    "cl: Device Not Found",
+	cl.CL_OUT_OF_HOST_MEMORY:  "cl: Out of Host Memory",
+	cl.CL_OUT_OF_RESOURCES:    "cl: Out of Resources",
+	cl.CL_INVALID_VALUE:       "cl: Invalid Value",
+	cl.CL_INVALID_PLATFORM:    "cl: Invalid Platform",
+	cl.CL_INVALID_DEVICE_TYPE: "cl: Invalid Device Type",
+}
+
+// Find the first device matching the device type from the list of platforms.
+func findDevice(platforms []cl.CL_platform_id, deviceType cl.CL_device_type) (platformID cl.CL_platform_id, deviceID cl.CL_device_id) {
+
+	// Search all platforms for the first device.
+	for _, platform := range platforms {
+
+		var numDevices cl.CL_uint
+
+		// Get the number of matching devices for the platform.
+		status := cl.CLGetDeviceIDs(
+			platform,
+			deviceType,
+			0,
+			nil,
+			&numDevices)
+
+		// Check for errors, continue to next platform if no matching device was found.
+		switch status {
+		case cl.CL_DEVICE_NOT_FOUND:
+			fallthrough
+		case cl.CL_SUCCESS:
+			if numDevices == 0 {
+				continue
+			}
+		default:
+			log.Printf("%s", deviceErrorMap[status])
+			log.Fatalf("Fatal error: could not retrieve devices for platform %d", platform)
+		}
+
+		device := make([]cl.CL_device_id, 1)
+
+		// Select first device matching the device type.
+		status = cl.CLGetDeviceIDs(
+			platform,
+			cl.CL_DEVICE_TYPE_GPU,
+			1,
+			device,
+			nil)
+
+		if status != cl.CL_SUCCESS {
+			log.Fatalf("Fatal error: could not retrieve GPU device for platform %d", platform)
+		}
+
+		platformID = platform
+		deviceID = device[0]
+		break
+	}
+
+	return
+}
+
+func parseCommandLine() {
+
+	flag.BoolVar(&useCPU, "cpu", false, "Whether to use the CPU over the GPU.")
+
+	flag.Parse()
+}
+
+func runOpenCL() {
+
 	var status cl.CL_int
 
 	var numPlatforms cl.CL_uint
@@ -76,31 +171,23 @@ func main() {
 	// Step 2: Discover and retrieve OpenCL devices.
 	//---------------------------------------------------
 
-	var numDevices cl.CL_uint
+	var preferredType cl.CL_device_type
 
-	status = cl.CLGetDeviceIDs(platforms[0], cl.CL_DEVICE_TYPE_ALL, 0, nil, &numDevices)
-
-	devices := make([]cl.CL_device_id, numDevices)
-
-	status = cl.CLGetDeviceIDs(
-		platforms[0],
-		cl.CL_DEVICE_TYPE_ALL,
-		numDevices,
-		devices,
-		nil)
-
-	if status != cl.CL_SUCCESS {
-		log.Fatalf("Fatal error: could not retrieve OpenCL device IDs.")
+	if useCPU {
+		preferredType = cl.CL_DEVICE_TYPE_CPU
+	} else {
+		preferredType = cl.CL_DEVICE_TYPE_GPU
 	}
 
-	// Debug log info.
-	log.Printf("Debug: found %d devices.", numDevices)
+	_, gpuDevice := findDevice(platforms, preferredType)
+	gpuDevices := make([]cl.CL_device_id, 1)
+	gpuDevices[0] = gpuDevice
 
 	//---------------------------------------------------
 	// Step 3: Create an OpenCL context.
 	//---------------------------------------------------
 
-	context := cl.CLCreateContext(nil, numDevices, devices, nil, nil, &status)
+	context := cl.CLCreateContext(nil, 1, gpuDevices, nil, nil, &status)
 
 	if status != cl.CL_SUCCESS {
 		log.Fatalf("Fatal error: could not create OpenCL context.")
@@ -112,7 +199,7 @@ func main() {
 	// Step 3: Create an OpenCL command queue.
 	//---------------------------------------------------
 
-	commandQueue := cl.CLCreateCommandQueue(context, devices[0], 0, &status)
+	commandQueue := cl.CLCreateCommandQueue(context, gpuDevice, 0, &status)
 
 	if status != cl.CL_SUCCESS {
 		log.Fatalf("Fatal error: could not create OpenCL command queue.")
@@ -126,7 +213,7 @@ func main() {
 
 	var kernelSource [1][]byte
 	var kernelLength [1]cl.CL_size_t
-	filename := "deceptive.cl"
+	filename := "kernels/deceptive.cl"
 
 	kernelData, err := ioutil.ReadFile(filename)
 
@@ -143,7 +230,7 @@ func main() {
 		log.Fatal("Fatal error: could not compile an OpenCL kernel from source.")
 	}
 
-	status = cl.CLBuildProgram(program, numDevices, devices, nil, nil, nil)
+	status = cl.CLBuildProgram(program, 1, gpuDevices, nil, nil, nil)
 
 	if status != cl.CL_SUCCESS {
 		log.Print("Fatal error: could not build OpenCL program.")
@@ -152,11 +239,11 @@ func main() {
 		var info interface{}
 
 		status = cl.CLGetProgramBuildInfo(
-			program, devices[0], cl.CL_PROGRAM_BUILD_LOG,
+			program, gpuDevice, cl.CL_PROGRAM_BUILD_LOG,
 			0, nil, &numChars)
 
 		status = cl.CLGetProgramBuildInfo(
-			program, devices[0], cl.CL_PROGRAM_BUILD_LOG,
+			program, gpuDevice, cl.CL_PROGRAM_BUILD_LOG,
 			numChars, &info, nil)
 
 		if status != cl.CL_SUCCESS {
@@ -166,33 +253,25 @@ func main() {
 		log.Fatalf("%s", info.(string))
 	}
 
-	kernel := cl.CLCreateKernel(program, []byte("deceptive"), &status)
+	kernel := cl.CLCreateKernel(program, []byte("gom"), &status)
 
 	if status != cl.CL_SUCCESS {
 		log.Fatal("Fatal error: could not create OpenCL kernel.")
 	}
 
 	//---------------------------------------------------
-	// Step 5: Initialize the LTGA.
-	//---------------------------------------------------
-
-	populationSize := 32
-	problemLength := 32
-
-	pop := NewPopulation(populationSize, problemLength)
-
-	freqs := Frequencies(pop)
-
-	lt := LinkageTree(pop, freqs)
-
-	fmt.Printf("%v\n", lt)
-
-	//---------------------------------------------------
 	// Step 6: Initialize OpenCL memory.
 	//---------------------------------------------------
 
+	populationSize := 32
+
 	var size cl.CL_uint
 	popSize := cl.CL_size_t(populationSize)
+	problemLength := 32
+	length := cl.CL_size_t(problemLength)
+
+	pop := NewPopulation(populationSize, problemLength)
+
 	dataSize := cl.CL_size_t(unsafe.Sizeof(size)) * popSize
 
 	populationData := make([]cl.CL_uint, populationSize)
@@ -220,6 +299,19 @@ func main() {
 		log.Fatal("Fatal error: could not allocate an OpenCL memory buffer.")
 	}
 
+	// Maximum bound on the number of elements in the LT + node sizes.
+	boundSum := (length*length+3*length-2)/2 + (2*length - 1)
+	ltSize := cl.CL_size_t(unsafe.Sizeof(length)) * boundSum
+
+	ltData := make([]cl.CL_uint, boundSum)
+
+	ltBuffer := cl.CLCreateBuffer(
+		context, cl.CL_MEM_READ_ONLY, ltSize, nil, &status)
+
+	if status != cl.CL_SUCCESS {
+		log.Fatal("Fatal error: could not allocate an OpenCL memory buffer.")
+	}
+
 	offspringBuffer := cl.CLCreateBuffer(
 		context, cl.CL_MEM_WRITE_ONLY, dataSize, nil, &status)
 
@@ -227,57 +319,113 @@ func main() {
 		log.Fatal("Fatal error: could not allocate an OpenCL memory buffer.")
 	}
 
-	status = cl.CLEnqueueWriteBuffer(
-		commandQueue, populationBuffer, cl.CL_TRUE, 0,
-		dataSize, unsafe.Pointer(&populationData[0]), 0, nil, nil)
-
-	if status != cl.CL_SUCCESS {
-		log.Fatal("Fatal error: could not write data to an OpenCL memory buffer.")
-	}
-
-	status = cl.CLSetKernelArg(
-		kernel, 0, cl.CL_size_t(unsafe.Sizeof(populationBuffer)),
-		unsafe.Pointer(&populationBuffer))
-
-	if status != cl.CL_SUCCESS {
-		fmt.Println(status)
-		log.Fatal("Fatal error: could not set arg 0 for OpenCL kernel.")
-	}
-
-	status = cl.CLSetKernelArg(
-		kernel, 1, cl.CL_size_t(unsafe.Sizeof(offspringBuffer)),
-		unsafe.Pointer(&offspringBuffer))
-
-	if status != cl.CL_SUCCESS {
-		log.Fatal("Fatal error: could not set arg 1 for OpenCL kernel.")
-	}
-
-	var globalWorkSize [1]cl.CL_size_t
-	globalWorkSize[0] = cl.CL_size_t(populationSize)
-
 	//---------------------------------------------------
-	// Step 7: Perform GOM crossover.
+	// Step 6: Perform GOMEA.
 	//---------------------------------------------------
 
-	status = cl.CLEnqueueNDRangeKernel(
-		commandQueue, kernel, 1, nil, globalWorkSize[:],
-		nil, 0, nil, nil)
+	rand.Seed(2343)
 
-	if status != cl.CL_SUCCESS {
-		log.Fatal("Fatal error: could not enqueue OpenCL kernel.")
+	done := false
+
+	for !done {
+
+		//---------------------------------------------------
+		// Step 5: Initialize the LTGA.
+		//---------------------------------------------------
+		freqs := Frequencies(pop)
+
+		lt := LinkageTree(pop, freqs)
+
+		// Store a flattened version of the linkage tree in memory.
+		ltPtr := 0
+		for _, node := range lt {
+			ltData[ltPtr] = cl.CL_uint(len(node))
+			for _, index := range node {
+				ltData[ltPtr] = cl.CL_uint(index)
+				ltPtr++
+			}
+		}
+		// null terminated
+		ltData[ltPtr] = 0
+
+		// Upload FOS.
+		status = cl.CLEnqueueWriteBuffer(
+			commandQueue, ltBuffer, cl.CL_TRUE, 0,
+			ltSize, unsafe.Pointer(&ltData[0]), 0, nil, nil)
+
+		if status != cl.CL_SUCCESS {
+			log.Fatal("Fatal error: could not write data to an OpenCL memory buffer.")
+		}
+
+		status = cl.CLEnqueueWriteBuffer(
+			commandQueue, populationBuffer, cl.CL_TRUE, 0,
+			dataSize, unsafe.Pointer(&populationData[0]), 0, nil, nil)
+
+		if status != cl.CL_SUCCESS {
+			log.Fatal("Fatal error: could not write data to an OpenCL memory buffer.")
+		}
+
+		status = cl.CLSetKernelArg(
+			kernel, 0, cl.CL_size_t(unsafe.Sizeof(populationBuffer)),
+			unsafe.Pointer(&populationBuffer))
+
+		if status != cl.CL_SUCCESS {
+			fmt.Println(status)
+			log.Fatal("Fatal error: could not set arg 0 for OpenCL kernel.")
+		}
+
+		status = cl.CLSetKernelArg(
+			kernel, 1, cl.CL_size_t(unsafe.Sizeof(ltBuffer)),
+			unsafe.Pointer(&ltBuffer))
+
+		if status != cl.CL_SUCCESS {
+			log.Fatal("Fatal error: could not set arg 1 for OpenCL kernel.")
+		}
+
+		status = cl.CLSetKernelArg(
+			kernel, 2, cl.CL_size_t(unsafe.Sizeof(offspringBuffer)),
+			unsafe.Pointer(&offspringBuffer))
+
+		if status != cl.CL_SUCCESS {
+			log.Fatal("Fatal error: could not set arg 2 for OpenCL kernel.")
+		}
+
+		var globalWorkSize [1]cl.CL_size_t
+		globalWorkSize[0] = cl.CL_size_t(populationSize)
+
+		//---------------------------------------------------
+		// Step 7: Perform GOM crossover.
+		//---------------------------------------------------
+
+		status = cl.CLEnqueueNDRangeKernel(
+			commandQueue, kernel, 1, nil, globalWorkSize[:],
+			nil, 0, nil, nil)
+
+		if status != cl.CL_SUCCESS {
+			log.Fatal("Fatal error: could not enqueue OpenCL kernel.")
+		}
+
+		cl.CLEnqueueReadBuffer(
+			commandQueue, offspringBuffer, cl.CL_TRUE, 0,
+			dataSize, unsafe.Pointer(&offspringData[0]), 0, nil, nil)
+
+		if status != cl.CL_SUCCESS {
+			log.Fatal("Fatal error: reading a buffer failed.")
+		}
+
+		for i, fitness := range offspringData {
+			log.Printf("dt(%-2d) = %-2d, bits = %032b, out(%-2d) = %-2d",
+				i, int(Evaluate(4, pop.Solutions[i].Bits)),
+				populationData[i], i, fitness)
+		}
+
+		done = true
 	}
+}
 
-	cl.CLEnqueueReadBuffer(
-		commandQueue, offspringBuffer, cl.CL_TRUE, 0,
-		dataSize, unsafe.Pointer(&offspringData[0]), 0, nil, nil)
+func main() {
 
-	if status != cl.CL_SUCCESS {
-		log.Fatal("Fatal error: reading a buffer failed.")
-	}
+	parseCommandLine()
 
-	for i, fitness := range offspringData {
-		log.Printf("dt(%-2d) = %-2d, bits = %032b, out(%-2d) = %-2d",
-			i, int(Evaluate(4, pop.Solutions[i].Bits)),
-			populationData[i], i, fitness)
-	}
+	runOpenCL()
 }
